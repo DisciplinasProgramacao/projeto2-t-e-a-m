ab5cfc4f6e841003833a1256c3df9f4eaee3f36d
André <afmendes@sga.pucminas.br>
2022-09-15 17:24:25 -0300

    Master

---
+++ codigo/src/ArquivoTextoLeitura.java
@@ -0,0 +1,39 @@
+public class ArquivoTextoLeitura {
+
+	private BufferedReader entrada;
+
+	ArquivoTextoLeitura(String nomeArquivo) {
+
+		try {
+			entrada = new BufferedReader(new FileReader(nomeArquivo));
+		} catch (FileNotFoundException excecao) {
+			System.out.println("Arquivo nao encontrado");
+		}
+	}
+
+	public void fecharArquivo() {
+
+		try {
+			entrada.close();
+		} catch (IOException excecao) {
+			System.out.println("Erro no fechamento do arquivo de leitura: " + excecao);
+		}
+	}
+
+	@SuppressWarnings("finally")
+	public String ler() {
+
+		String textoEntrada = null;
+
+		try {
+			textoEntrada = entrada.readLine();
+		} catch (EOFException excecao) { // Excecao de final de arquivo.
+			textoEntrada = null;
+		} catch (IOException excecao) {
+			System.out.println("Erro de leitura: " + excecao);
+			textoEntrada = null;
+		} finally {
+			return textoEntrada;
+		}
+	}
+}
