5b28bb770a9cbe1b73752c2cfe52566c86f0f07b
André <afmendes@sga.pucminas.br>
2022-09-22 10:41:20 -0300

    adequar codigo a UML

---
+++ codigo/src/GrafoMutavel.java
@@ -0,0 +1,47 @@
+public class GrafoMutavel extends Grafo {
+
+    /**
+     * Adiciona uma aresta entre dois vértices do grafo.
+     * Não verifica se os vértices pertencem ao grafo.
+     * 
+     * @param origem  Vértice de origem
+     * @param destino Vértice de destino
+     */
+    public boolean addAresta(int origem, int destino) {
+        boolean adicionou = false;
+        Vertice saida = this.existeVertice(origem);
+        Vertice chegada = this.existeVertice(destino);
+        if (saida != null && chegada != null) {
+            saida.addAresta(destino);
+            chegada.addAresta(origem);
+            adicionou = true;
+        }
+
+        return adicionou;
+
+    }
+
+    /**
+     * Adiciona, se possível, um vértice ao grafo. O vértice é auto-nomeado com o
+     * próximo id disponível.
+     */
+    public boolean addVertice(int id) {
+        Vertice novo = new Vertice(id);
+        return this.vertices.add(id, novo);
+    }
+
+    public void carregar(String nomeArquivo) {
+    }
+
+    public boolean delAresta(int origem, int destino) {
+
+    }
+
+    public boolean delVertice(int idVertice) {
+
+    }
+
+    public void salvar(String nomeArquivo) {
+    }
+
+}
--- codigo/src/Vertice.java
+++ codigo/src/Vertice.java
@@ -1,4 +1,4 @@
-/** 
+/**
  * MIT License
  *
  * Copyright(c) 2021 João Caram <caram@pucminas.br>
@@ -10,7 +10,8 @@
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
+ * The above copyright notice and this permission notice shall be included in
+ * all
  * copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
@@ -23,65 +24,70 @@
  */
 
 public class Vertice {
-    
-    private ABB<Aresta> arestas;    
-    private final int  id;
+
+    private final int id;
+    private ABB<Aresta> arestas;
     private boolean visitado;
 
-/**
+    /**
      * Construtor para criação de vértice identificado
+     * 
      * @param id Número/id do vértice a ser criado (atributo final).
      */
-    public Vertice(int id){
+    public Vertice(int id) {
         this.id = id;
         this.arestas = new ABB<Aresta>();
         this.visitado = false;
     }
-    
-  /**
+
+    /**
      * Adiciona uma aresta neste vértice para um destino
+     * 
      * @param peso Peso da aresta (1 para grafos não ponderados)
      * @param dest Vértice de destino
      */
-    public boolean addAresta(int destino){
-        return this.arestas.add(destino,new Aresta(0, destino));
+    public boolean addAresta(int destino) {
+        return this.arestas.add(destino, new Aresta(0, destino));
     }
 
     /**
      * Verifica se já existe aresta entre este vértice e um destino. Método privado
+     * 
      * @param dest Vértice de destino
      * @return TRUE se existe aresta, FALSE se não
      */
-    public Aresta existeAresta(int destino){
+    public Aresta existeAresta(int destino) {
         return this.arestas.find(destino);
 
     }
 
-    public Aresta arestaApontandoPara(int destino){
-        if(existeAresta(destino) != null){
+    public Aresta arestaApontandoPara(int destino) {
+        if (existeAresta(destino) != null) {
             return existeAresta(destino);
         }
 
         return null;
     }
-    
+
     /**
      * Retorna o grau do vértice
+     * 
      * @return Grau (inteiro não negativo)
      */
-    public int grau(){
+    public int grau() {
         return this.arestas.size();
     }
 
-    public void visitar(){
-        this.visitado = true;
-    }
-    
-    public void limparVisita(){
+    public void limparVisita() {
         this.visitado = false;
     }
-    
-    public boolean visitado(){
+
+    public boolean visitado() {
         return this.visitado;
     }
+
+    public void visitar() {
+        this.visitado = true;
+    }
+
 }
